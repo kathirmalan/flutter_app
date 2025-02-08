16b6c674f18a496ae15b74b14c06e1c6ea0c6aed
Kathirmalan SHANMUGAM <kathirmalan.shanmugam@ibrowse.com>
2025-02-08 01:28:53 +0100

    button type update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class IBSoftPhone extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: DialPad(),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue
       ),
     );
   }
@@ -35,57 +35,161 @@ class DialPad extends StatelessWidget {
       body: Center(
           child: Container(
               padding: const EdgeInsets.fromLTRB(4.0, 34.0, 4.0, 4.0),
-              margin: const EdgeInsets.fromLTRB(0, 180, 0, 0),
+              margin: const EdgeInsets.fromLTRB(0, 90, 0, 0),
               child: Column(
                 children: [
                   Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('1'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('2'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('3'),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('4'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('5'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('6'),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('7'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('8'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('9'),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('+'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('0'),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Text('#'),
+                        ),
+                      ]
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
                         FilledButton(
-                            onPressed: () {},
-                            child: const Text('1')),
-                        FilledButton(onPressed: () {}, child: const Text('2')),
-                        FilledButton(onPressed: () {}, child: const Text('3')),
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: Colors.blue.shade900,
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(34),
+                          ),
+                          child: Icon(Icons.call),
+                        )
                       ]
                   ),
-                  SizedBox(height: 40),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        FilledButton(onPressed: () {}, child: const Text('4')),
-                        FilledButton(onPressed: () {}, child: const Text('5')),
-                        FilledButton(onPressed: () {}, child: const Text('6')),
-                      ]
-                  ),
-                  SizedBox(height: 40),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        FilledButton(onPressed: () {}, child: const Text('7')),
-                        FilledButton(onPressed: () {}, child: const Text('8')),
-                        FilledButton(onPressed: () {}, child: const Text('9')),
-                      ]
-                  ),
-                  SizedBox(height: 40),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        FilledButton(onPressed: () {}, child: const Text('*')),
-                        FilledButton(onPressed: () {}, child: const Text('0')),
-                        FilledButton(onPressed: () {}, child: const Text('#')),
-                      ]
-                  )
+
                 ],
               ))),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue.shade900,
-        onPressed: () {
-          print("pressed");
-        },
-        child: Text(
-          "Call",
-          style: TextStyle(color: Colors.white),
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.offline_bolt),
+            label: 'Screen 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.panorama_fish_eye),
+            label: 'Screen 3',
+          ),
+        ],
       ),
     );
   }
